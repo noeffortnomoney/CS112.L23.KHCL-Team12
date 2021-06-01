cbd26c302c9a582cc6f971eaa1ded1f89bbf0188
Trần Vĩ Hào <tranvihao1412@gmail.com>
2021-06-01 10:30:14 +0700

    Add files via upload

---
+++ Project/Phần làm BTVN của nhóm 12/Nhóm 9 - Sodoku.py
@@ -0,0 +1,43 @@
+N = 9
+def printing(arr):
+    for i in range(N):
+        for j in range(N):
+            print(arr[i][j], end = " ")
+        print()
+def isSafe(grid, row, col, num):
+    for x in range(9):
+        if grid[row][x] == num:
+            return False
+    for x in range(9):
+        if grid[x][col] == num:
+            return False
+    startRow = row - row % 3
+    startCol = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if grid[i + startRow][j + startCol] == num:
+                return False
+    return True
+def solveSuduko(grid, row, col):
+    if (row == N - 1 and col == N):
+        return True
+    if col == N:
+        row += 1
+        col = 0
+    if grid[row][col] > 0:
+        return solveSuduko(grid, row, col + 1)
+    for num in range(1, N + 1, 1):
+        if isSafe(grid, row, col, num):
+            grid[row][col] = num
+            if solveSuduko(grid, row, col + 1):
+                return True
+        grid[row][col] = 0
+    return False
+grid = [[]]*9
+for i in range(9):
+	grid[i] = [(int(x)) for x in input().split()]
+print()
+if (solveSuduko(grid, 0, 0)):
+    printing(grid)
+else:
+    print("no solution  exists ")
