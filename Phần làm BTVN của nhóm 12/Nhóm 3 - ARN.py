4916acff5566d21c4d7bb504ec5bf03380185a1b
Bình Quốc Trương <79963001+noeffortnomoney@users.noreply.github.com>
2021-06-25 20:59:08 +0700

    Add files via upload

---
+++ Phần làm BTVN của nhóm 12/Nhóm 3 - ARN.py
@@ -0,0 +1,17 @@
+
+def toString(List):
+	return ''.join(List)
+def permute(a, l, r):
+    if l==r:
+     print(toString(a))
+    else:
+     for i in range(l,r+1):
+      a[l], a[i] = a[i], a[l]
+      permute(a, l+1, r)
+      a[l], a[i] = a[i], a[l]
+
+sum=0
+string = "ATG"
+n = len(string)
+a = list(string)
+permute(a, 0, n-1)
